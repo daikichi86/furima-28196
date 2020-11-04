7a56c52d8426c91b274c3c71df291779c524b448
daikichi86 <maskman-daikigx@i.softbank.jp>
2020-11-04 15:09:19 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,13 @@
 class Item < ApplicationRecord
   has_one_attached :image
-  
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :sales_status, :shipping_fee_status, :prefecture, :scheduled_delivery
+
+  validates :category_id, numericality: { other_than: 1 }
+  validates :sales_status_id, numericality: { other_than: 1 }
+  validates :shipping_fee_status_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :scheduled_delivery_id, numericality: { other_than: 1 }
+ 
 end
