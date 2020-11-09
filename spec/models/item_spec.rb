60f311080829afbb31ae6dfe193c12143267d0be
daikichi86 <maskman-daikigx@i.softbank.jp>
2020-11-09 20:37:25 +0900

    修正3

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Item, type: :model do
 
   describe '商品出品機能' do
     
-    it "全ての条件を満たしていればツイートは保存される" do
+    it "全ての条件を満たしていれば商品はは出品できる" do
       expect(@item).to be_valid
     end
 
@@ -29,10 +29,22 @@ RSpec.describe Item, type: :model do
       expect(@item.errors.full_messages).to include("Info can't be blank")
     end
 
+    it "category_idが空だと登録できない" do
+      @item.category_id = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category Select")
+    end
+
     it "category_idを選択しないと登録できない" do
       @item.category_id = "1"
       @item.valid?
       expect(@item.errors.full_messages).to include("Category Select")
+    end
+    
+    it "sales_status_idが空だと登録できない" do
+      @item.sales_status_id = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Sales status Select")
     end
 
     it "sales_status_idを選択しないと登録できない" do
@@ -40,17 +52,35 @@ RSpec.describe Item, type: :model do
       @item.valid?
       expect(@item.errors.full_messages).to include("Sales status Select")
     end
+    
+    it "shipping_fee_status_idが空だと登録できない" do
+      @item.shipping_fee_status_id = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shipping fee status Select")
+    end
 
     it "shipping_fee_status_idを選択しないと登録できない" do
       @item.shipping_fee_status_id = "1"
       @item.valid?
       expect(@item.errors.full_messages).to include("Shipping fee status Select")
     end
+    
+    it "prefecture_idが空だと登録できない" do
+      @item.prefecture_id = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Prefecture Select")
+    end
 
     it "prefecture_idを選択しないと登録できない" do
       @item.prefecture_id = "1"
       @item.valid?
       expect(@item.errors.full_messages).to include("Prefecture Select")
+    end
+    
+    it "scheduled_delivery_idが空だと登録できない" do
+      @item.scheduled_delivery_id = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Scheduled delivery Select")
     end
 
     it "scheduled_delivery_idを選択しないと登録できない" do
